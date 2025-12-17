2382fda0525b85dc2348d6582c1315df23a725f5
bala-amypo <bala@amypo.in>
2025-12-17 09:35:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -23,7 +23,7 @@ public class Student{
         return name;
     }
     public void setName(String name){
-        this.id=id;
+        this.name=name;
     }
     public String getdept(){
         return dept;
