eb7ded71100d2f345e76fe1704ce3084765a10ff
bala-amypo <bala@amypo.in>
2025-12-18 04:46:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,24 +1,23 @@
 package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.*;
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.Student;
-
+import com.example.demo.service.StudentService;
 @RestController
-public class StudentController{
+public class StudentController {
     @Autowired
     StudentService stdser;
     @PostMapping("/addStudent")
     public Student addStudent(@RequestBody Student st){
-        return stdser.poststudent(st);
+        return stdser.postStudent(st);
     }
-    @GetMapping("/getall")
-    public List<Student>get(){
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents() {
         return stdser.getAllStudents();
     }
     @GetMapping("/getById/{id}")
-    public Optional<Student>getId(@pathVariable Long id){
+    public Optional<Student> getId(@PathVariable Long id){
         return stdser.getById(id);
     }
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -2,9 +2,8 @@ package com.example.demo.service;
 import java.util.*;
 import com.example.demo.entity.Student;
 
-
 public interface StudentService{
-    Student poststudent(Student st);
+    Student postStudent(Student st);
     List<Student> getAllStudents();
     Optional<Student> getById(Long id);
 }
